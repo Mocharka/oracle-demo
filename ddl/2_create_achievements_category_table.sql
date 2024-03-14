2d44c753c521958d12d4d9cc90416ae0df67b17a
Matster07 <60222325+Matster07@users.noreply.github.com>
2024-03-14 23:42:14 +0300

    Update 2_create_achievements_category_table.sql

--- ddl/2_create_achievements_category_table.sql
+++ ddl/2_create_achievements_category_table.sql
@@ -3,5 +3,5 @@ CREATE SEQUENCE achievements_category_seq;
 CREATE TABLE achievements_category (
    id NUMBER DEFAULT achievements_category_seq.nextval PRIMARY KEY,
    category_name VARCHAR2(200) NOT NULL,
-   created_at TIMESTAMP
+   created_at TIMESTAMP DEFAULT current_timestamp NOT NULL
 );
