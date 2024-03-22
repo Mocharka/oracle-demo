f1b28c49c5354472bd0188f8fd1526c456eeee82
Mocharka <76014891+Mocharka@users.noreply.github.com>
2024-03-22 21:05:56 +0300

    Add files via upload

---
+++ insert/Achievements.sql
@@ -0,0 +1,12 @@
+select * from achievements;
+
+INSERT INTO achievements (achievements_category_id, user_id, proof_url, is_approved)
+VALUES ('1','1','proof1','true');
+INSERT INTO achievements (achievements_category_id, user_id, proof_url, is_approved)
+VALUES ('3','2','proof2','true');
+
+INSERT INTO achievements (achievements_category_id, user_id, proof_url, is_approved)
+VALUES ('3','3','proof3','true');
+
+INSERT INTO achievements (achievements_category_id, user_id, proof_url, is_approved)
+VALUES ('4','3','proof3','true');
---
+++ insert/Users.sql
@@ -0,0 +1,13 @@
+selecct * from users;
+
+INSERT INTO USERS (full_name, proof_url, birthday, photo_url, group_number)
+VALUES ('Eugeny','proof1','December 12','photo1', 'RT2');
+
+INSERT INTO USERS (full_name, proof_url, birthday, photo_url, group_number)
+VALUES ('Ilya','proof2','April 24','photo2', 'RT2');
+
+INSERT INTO USERS (full_name, proof_url, birthday, photo_url, group_number)
+VALUES ('Matvey','proof3','October 7','photo3', 'RT2');
+
+INSERT INTO USERS (full_name, proof_url, birthday, photo_url, group_number)
+VALUES ('Alexey','proof4','January 01','photo4', 'RT5');
